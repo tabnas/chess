751c9a807340ff7628df7f05f3bfd174fc482c87
Richard Rodger <richard@ricebridge.com>
2026-08-21 02:33:43 +0100

    release: v0.1.6 (unify go+ts, patch bump)

--- ts/src/chess.ts
+++ ts/src/chess.ts
@@ -263,7 +263,7 @@ const grammarText = `
 }`
 // --- END EMBEDDED chess-grammar.jsonic ---
 
-export const VERSION = '0.1.5'
+export const VERSION = '0.1.6'
 
 // --- The parse model -----------------------------------------------------
 
